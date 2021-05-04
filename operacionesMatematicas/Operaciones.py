969b4f64055439a07474b8c65e943c42572c0f21
Emanuel Tort <etortc@hotmail.com>
2021-05-04 18:05:18 -0500

    operaciones
    suma imput ocnvertido

---
+++ operacionesMatematicas/Operaciones.py
@@ -0,0 +1,11 @@
+# Entrada de datos
+numero1 = float(input("Escribe un numero: "))
+numero2 = float(input("Escribe otro numero: "))
+
+
+# calculos y/o operaciones
+suma = (numero1 + numero2)
+
+#salida de datos
+print("suma = " + str(suma))
+print(f"suma =  {suma}")
